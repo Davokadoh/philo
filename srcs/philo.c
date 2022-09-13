f871e4ca53d7eabb78fc6cc15a362e15b6a64eb8
Joachim Leroux <jleroux@c2r6s5.42lausanne.ch>
2022-09-13 10:57:17 +0200

    Philo eat,sleep,think but never die

--- srcs/philo.c
+++ srcs/philo.c
@@ -6,136 +6,153 @@
 /*   By: jleroux <marvin@42lausanne.ch>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 13:11:47 by jleroux           #+#    #+#             */
-/*   Updated: 2022/09/12 17:01:15 by jleroux          ###   ########.fr       */
+/*   Updated: 2022/09/13 10:56:39 by jleroux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 
-#define	EATING 0
-#define	SLEEPING 1
-
-typedef struct	s_rules
+int	ft_error(char *msg)
 {
-	int	philo_nbr;
-	int	death_time;
-	int	eat_time;
-	int	sleep_time;
-	int	meal_nbr;
-}				t_rules;
-
-typedef struct	s_philo
-{
-	int				i;
-	int				state;
-	int				meals_eaten;
-	t_rules			rules;
-	pthread_mutex_t	*forks;
-}				t_philo;
-
-void	take_fork(t_philo philo, int frk, pthread_mutex_t *forks)
-{
-	if (frk >= philo.rules.philo_nbr)
-		frk = 0;
-	//printf("Philo %i tries to take fork %i\n", philo.i, frk);
-	pthread_mutex_lock(&forks[frk]);
-	printf("Philo %i has taken fork %i\n", philo.i, frk);
+	printf("%s\n", msg);
+	return (1);
 }
 
-void	eat(t_philo *philo, pthread_mutex_t *forks)
+void	take_fork(t_ph ph, int frk, pthread_mutex_t *frks)
 {
-	//if (philo->i == philo->rules.philo_nbr - 1)
-	if (philo->i % 2 == 0)
+	if (frk >= ph.rules.philo_nbr)
+		frk = 0;
+	pthread_mutex_lock(&frks[frk]);
+	printf("Philo %i has taken fork %i\n", ph.id + 1, frk + 1);
+}
+
+void	eat(t_ph *ph, pthread_mutex_t *frks)
+{
+	if (ph->id % 2 == 0)
 	{
-		take_fork(*philo, philo->i + 1, forks);
-		take_fork(*philo, philo->i, forks);
+		take_fork(*ph, ph->id + 1, frks);
+		take_fork(*ph, ph->id, frks);
 	}
 	else
 	{
-		take_fork(*philo, philo->i, forks);
-		take_fork(*philo, philo->i + 1, forks);
+		take_fork(*ph, ph->id, frks);
+		take_fork(*ph, ph->id + 1, frks);
 	}
-	printf("Philo %i is eating\n", philo->i);
-	usleep(philo->rules.eat_time);
-	pthread_mutex_unlock(&forks[philo->i]);
-	if (philo->i + 1 >= philo->rules.philo_nbr)
-		pthread_mutex_unlock(&forks[0]);
+	printf("Philo %i is eating\n", ph->id + 1);
+	usleep(ph->rules.eat_time);
+	ph->meals_eaten++;
+	pthread_mutex_unlock(&frks[ph->id]);
+	if (ph->id + 1 >= ph->rules.philo_nbr)
+		pthread_mutex_unlock(&frks[0]);
 	else
-		pthread_mutex_unlock(&forks[philo->i + 1]);
+		pthread_mutex_unlock(&frks[ph->id + 1]);
 }
 
 void	zzz(int time, int i)
 {
 	(void)i;
-	//printf("Philo %i is sleeping\n", i);
+	printf("Philo %i is sleeping\n", i + 1);
 	usleep(time);
-	//printf("Philo %i is thinking\n", i);
+	printf("Philo %i is thinking\n", i + 1);
 }
 
-void	*philo_thread(void *void_philo)
+void	*routine(void *void_philo)
 {
-	t_philo	*philo;
+	t_ph	*ph;
 
-	philo = (t_philo *) void_philo;
-	philo->meals_eaten = 3;
-	while (philo->meals_eaten)
+	ph = (t_ph *) void_philo;
+	while (ph->meals_eaten < ph->rules.max_meal || ph->rules.max_meal == 0)
 	{
-		eat(philo, philo->forks);
-		zzz(philo->rules.sleep_time, philo->i);
-		philo->meals_eaten--;
+		eat(ph, ph->frks);
+		zzz(ph->rules.sleep_time, ph->id);
 	}
 	pthread_exit(NULL);
+}
+
+void	threads(t_ph *ph, t_rules rules, pthread_mutex_t *frks, pthread_t *tids)
+{
+	int	i;
+
+	printf("test\n");
+	i = -1;
+	while (++i < rules.philo_nbr)
+	{
+		ph[i].id = i;
+		ph[i].rules = rules;
+		ph[i].frks = frks;
+		ph[i].meals_eaten = 0;
+		pthread_create(&tids[i], NULL, routine, (void *) &ph[i]);
+	}
+}
+
+void	wait_then_dtry_mutexes(int max, pthread_t *tids, pthread_mutex_t *frks)
+{
+	int	i;
+
+	i = -1;
+	while (++i < max)
+		pthread_join(tids[i], NULL);
+	i = -1;
+	while (++i < max)
+		pthread_mutex_destroy(&frks[i]);
+}
+
+void	free_arrays(t_ph *ph, pthread_t *tids, pthread_mutex_t *frks)
+{
+	free(tids);
+	free(frks);
+	free(ph);
+}
+
+void	init_mutexes(int max, pthread_mutex_t *frks)
+{
+	int	i;
+
+	i = -1;
+	while (++i < max)
+		pthread_mutex_init(&frks[i], NULL);
+}
+
+int	malloc_arrays(int nbr, t_ph **ph, pthread_t **tids, pthread_mutex_t **frks)
+{
+	*ph = malloc(nbr * sizeof(t_ph));
+	*tids = malloc(nbr * sizeof(pthread_t));
+	*frks = malloc(nbr * sizeof(pthread_mutex_t));
+	if (!*ph || !*tids || !*frks)
+		return (1);
 	return (0);
 }
 
-void	get_rules(int ac, char *av[], t_rules *rules)
+t_rules	get_rules(int ac, char *av[])
 {
-	if (ac != 5 && ac != 6)
-	{
-		printf("Wrong arguments!\n");
-		return ;
-	}
-	rules->philo_nbr = ft_atoi(av[1]);
-	rules->death_time = ft_atoi(av[2]);
-	rules->eat_time = ft_atoi(av[3]);
-	rules->sleep_time = ft_atoi(av[4]);
+	t_rules	rules;
+
+	rules.philo_nbr = ft_atoi(av[1]);
+	rules.death_time = ft_atoi(av[2]);
+	rules.eat_time = ft_atoi(av[3]);
+	rules.sleep_time = ft_atoi(av[4]);
 	if (ac == 6)
-		rules->meal_nbr = ft_atoi(av[5]);
+		rules.max_meal = ft_atoi(av[5]);
+	else
+		rules.max_meal = 0;
+	return (rules);
 }
 
 int	main(int ac, char *av[])
 {
-	int	i;
-	pthread_t	*threads;
-	pthread_mutex_t	*forks;
-	t_philo	*philo;
-	t_rules rules;
+	t_rules			rules;
+	t_ph			*ph;
+	pthread_t		*tids;
+	pthread_mutex_t	*frks;
 
-	get_rules(ac, av, &rules);
-	threads = malloc(rules.philo_nbr * sizeof(pthread_t));
-	forks = malloc(rules.philo_nbr * sizeof(pthread_mutex_t));
-	philo = malloc(rules.philo_nbr * sizeof(t_philo));
-	i = -1;
-	while (++i < rules.philo_nbr)
-		pthread_mutex_init(&forks[i], NULL);
-	i = -1;
-	while (++i < rules.philo_nbr)
-	{
-		philo[i].i = i;
-		philo[i].rules = rules;
-		philo[i].forks = forks;
-        pthread_create(&threads[i], NULL, philo_thread, (void*) &philo[i]);
-	}
-	i = -1;
-	while (++i < rules.philo_nbr)
-    	pthread_join(threads[i], NULL);
-	pthread_mutex_destroy(&forks[i]);
-	free(threads);
-	free(forks);
-	free(philo);
+	if (ac != 5 && ac != 6)
+		return (ft_error("Wrong number of arguments"));
+	rules = get_rules(ac, av); //Check if args are all ints
+	if (malloc_arrays(rules.philo_nbr, &ph, &tids, &frks))
+		return (ft_error("malloc fail"));
+	init_mutexes(rules.philo_nbr, frks); //Protect mutexes ?
+	threads(ph, rules, frks, tids); //Check if tids created ?
+	wait_then_dtry_mutexes(rules.philo_nbr, tids, frks); //Nothing to be done ?
+	free_arrays(ph, tids, frks); //Check if arrays exist before freeing
 	return (0);
 }
--- srcs/philo.h
+++ srcs/philo.h
@@ -6,12 +6,39 @@
 /*   By: jleroux <marvin@42lausanne.ch>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 13:13:22 by jleroux           #+#    #+#             */
-/*   Updated: 2022/09/09 13:14:39 by jleroux          ###   ########.fr       */
+/*   Updated: 2022/09/13 10:55:06 by jleroux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+//Change to enum ?
+# define EATING 0
+# define SLEEPING 1
+
+typedef struct s_rules
+{
+	int	philo_nbr;
+	int	death_time;
+	int	eat_time;
+	int	sleep_time;
+	int	max_meal;
+}				t_rules;
+
+typedef struct s_philo
+{
+	int				id;
+	int				state;
+	int				meals_eaten;
+	t_rules			rules;
+	pthread_mutex_t	*forks;
+}				t_ph;
 
 int	ft_atoi(const char *str);
 
